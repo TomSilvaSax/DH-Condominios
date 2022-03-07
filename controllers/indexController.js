6fc87f8ee369f9d8949c4dd586657663c6c65d34
Tom Silva <antoniosantana.elonet@gmail.com>
2022-03-07 19:43:44 -0300

    Primeiro commit

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,9 +3,12 @@ var page = require('../models/page')
 
 
 const controller = {
-    index: function(req, res, next) {
-        res.render('index', page);
+    index: function(req, res, next) { res.render('index', page);}
+   
+
       }
-}
+    
+
+    
 
 module.exports = controller
--- routes/index.js
+++ routes/index.js
@@ -14,4 +14,5 @@ router.get('/sobre', function(req, res){
 });
 
 
+
 module.exports = router;
